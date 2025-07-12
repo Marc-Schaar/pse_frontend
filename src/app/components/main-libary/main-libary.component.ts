4bb7a6bbf064538dfe96e4a2cb45f5b63405e8e3
Marc Schaar <m-schaar@gmx.net>
2025-07-12 23:24:13 +0200

    feat: add search by name or  ordnungszahl

--- src/app/components/main-libary/main-libary.component.ts
+++ src/app/components/main-libary/main-libary.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ApiService } from '../../services/api.service';
 
@@ -9,27 +10,54 @@ import { ElementCardDialogComponent } from '../element-card-dialog/element-card-
 
 @Component({
   selector: 'app-main-libary',
-  imports: [CommonModule, ElementCardComponent, ElementCardDialogComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ElementCardComponent,
+    ElementCardDialogComponent,
+  ],
   templateUrl: './main-libary.component.html',
   styleUrl: './main-libary.component.scss',
 })
 export class MainLibaryComponent implements OnInit {
   private readonly dbService: ApiService = inject(ApiService);
-
   private databasePseElements: PseElement[] = [];
   private pseElementsTorRender: PseElement[] = [];
   private dialogOpen: boolean = false;
-  private elementDetail: number | null = null;
+  public searchValue: string = '';
 
   ngOnInit() {
     this.dbService.loadAllfromDb().subscribe((data) => {
       this.databasePseElements = data;
-      this.pseElementsTorRender = [...this.databasePseElements];
+      this.renderDbElements();
     });
+  }
+
+  private renderDbElements() {
+    return (this.pseElementsTorRender = [...this.databasePseElements]);
   }
 
   public getPseElements() {
     return this.pseElementsTorRender;
+  }
+
+  public searchElementInit() {
+    if (!this.searchValue.trim()) this.renderDbElements();
+    let isNum = Number(this.searchValue);
+    let result = isNum ? this.searchByOrdnungszahl() : this.searchByName();
+    this.pseElementsTorRender = [...result];
+  }
+
+  private searchByOrdnungszahl(): PseElement[] {
+    return this.databasePseElements.filter(
+      (e) => e.ordnungszahl === +this.searchValue
+    );
+  }
+
+  private searchByName(): PseElement[] {
+    return this.databasePseElements.filter((e) =>
+      e.name.toLocaleLowerCase().includes(this.searchValue)
+    );
   }
 
   public handleElementClick(id: number) {
@@ -46,10 +74,8 @@ export class MainLibaryComponent implements OnInit {
     this.dbService.removeQueryParam();
   }
 
-  getGridColumn(element: PseElement): number | null {
-    if (element.gruppe == null) {
-      return null;
-    } else return element.gruppe;
+  getGridColumn(element: PseElement): number {
+    return element.gruppe;
   }
 
   getGridRow(element: PseElement): number {
